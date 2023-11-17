222e73f1533480d2d9dc045c43f4bb2c52578086
Jeon <gino9940@gmail.com>
2023-11-17 16:12:06 +0900

    feat: add subparser example

---
+++ commands/run.py
@@ -0,0 +1,12 @@
+
+def run():
+    questions = [
+        {
+            'type': 'input',
+            'name': 'image',
+            'message': 'Enter the Docker image:',
+        },
+        # 추가적인 질문들...
+    ]
+
+    print(questions)
---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+# import docker
+
+from commands.run import run
+
+COMMANDS = {
+    'ADD': 'add',  # example
+    'RUN': 'run',
+    'EXEC': 'exec',
+    'BUILD': 'build'
+}
+
+
+def main():
+    # client = docker.from_env()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command')
+
+    add_parser = subparsers.add_parser(COMMANDS['ADD'], help='Add two numbers')
+    add_parser.add_argument('num1', type=int)
+    add_parser.add_argument('num2', type=int)
+
+    run_parser = subparsers.add_parser(
+        COMMANDS['RUN'], help='Create Container')
+
+    args = parser.parse_args()
+
+    if args.command == COMMANDS['ADD']:
+        result = args.num1 + args.num2
+        print(f'The sum is: {result}')
+    elif args.command == COMMANDS['RUN']:
+        run()
+
+
+if __name__ == '__main__':
+    main()
