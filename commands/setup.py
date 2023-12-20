ab6c06b0272dda107ff6ffcdd2e19f2d7f4e4e40
kang yeon joo <rkdduswndi@naver.com>
2023-12-20 18:21:46 +0900

    feat: top 명령어 돌아가도록 구현

---
+++ commands/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+from io import open
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='ctop',
+    version='1.0.0',
+    description='A lightweight top like monitor for linux CGroups',
+    long_description=readme,
+    author='Jean-Tiare Le Bigot',
+    author_email='jt@yadutaf.fr',
+    url='https://github.com/yadutaf/ctop',
+    py_modules=['cgroup_top'],
+    scripts=['bin/ctop'],
+    license='MIT',
+    platforms = 'any',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
